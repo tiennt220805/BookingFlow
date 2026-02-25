74760e18f141a624d27ab82066f476cd98154a2e
thanhtien2208 <tiennt220805@gmail.com>
2026-02-26 00:30:46 +0700

    feat: add Introduction Screen

---
+++ app/src/main/java/com/tipie/bookingflow/BaseActivity.java
@@ -0,0 +1,22 @@
+package com.tipie.bookingflow;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+}
---
+++ app/src/main/java/com/tipie/bookingflow/IntroActivity.java
@@ -0,0 +1,34 @@
+package com.tipie.bookingflow;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.tipie.bookingflow.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends BaseActivity {
+
+    private ActivityIntroBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
+
+
+    }
+}
